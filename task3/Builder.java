70286405ebc7e56152b2cdcc49bb655737daf259
farida-tagayeva <74818678+farida-tagayeva@users.noreply.github.com>
2021-03-13 14:52:46 +0600

    Update Builder.java

--- task3/Builder.java
+++ task3/Builder.java
@@ -1,4 +1,10 @@
 package task3;
 
 public interface Builder {
+   public void buildHouseWalls();
+    public void buildHouseDoors();
+    public void buildHouseWindows();
+    public void buildHouseRoof();
+    public void buildHouseGarage();
+    public House getHouse();
 }
