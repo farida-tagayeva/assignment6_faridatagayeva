c40aaf174d9c08bc766a059ced7feb1686176d9a
farida-tagayeva <74818678+farida-tagayeva@users.noreply.github.com>
2021-03-13 14:52:09 +0600

    Update Application.java

--- task3/Application.java
+++ task3/Application.java
@@ -1,4 +1,13 @@
 package task3;
 
 public class Application {
+  public static void main(String[] args) {
+        Builder builder = new Builder();
+        Director directr = new Director(Builder);
+        director.Builder();
+        House house = director.getHouse();
+        System.out.println(house);
+      
+       
+    }
 }
