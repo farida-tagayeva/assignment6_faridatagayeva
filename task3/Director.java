af68588a89f54bb675423743d49d71f88d843123
farida-tagayeva <74818678+farida-tagayeva@users.noreply.github.com>
2021-03-13 14:53:29 +0600

    Update Director.java

--- task3/Director.java
+++ task3/Director.java
@@ -1,4 +1,18 @@
 package task3;
 
 public class Director {
+   private Builder builder;
+    public Director(Builder builder){
+        this.builder=builder;
+    }
+    public House getHouse(){
+        return this.builder.getHouse();
+    }
+    public void houseBuild(){
+        this.builder.buildHouseDoors();
+        this.builder.buildHouseWalls();
+        this.builder.buildHouseWindows();
+        this.builder.buildHouseRoof();
+        this.builder.buildHouseGarage();
+    }
 }
